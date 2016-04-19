9cb4449ac1a47a69d6671325b733e61aca7d8415
Brian Downing <bdowning@lavos.net>
2016-04-19 17:45:30 -0500

    Switch from Chai to expect

--- test/createInterpolator.spec.js
+++ test/createInterpolator.spec.js
@@ -1,10 +1,10 @@
-import { expect } from 'chai';
+import expect from 'expect';
 import { createInterpolator, identity } from '../src/index';
 
 describe('createInterpolator', () => {
   it('exposes the public API', () => {
     const interpolator = createInterpolator({ }, { });
 
-    expect(interpolator).to.be.a('function');
+    expect(interpolator).toBeA('function');
   });
 });
