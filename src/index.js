9c8f66fb5c5fc296d53e4863dbd63d0f0ee6e0c2
Brian Downing <bdowning@lavos.net>
2016-04-19 18:17:29 -0500

    Easing functions, basic error handling

--- src/index.js
+++ src/index.js
@@ -7,9 +7,23 @@ function interpolate(from, to, position) {
 }
 
 export function createInterpolator(state, easing) {
+  for (let prop in state) {
+    if (typeof state[prop] !== 'number') {
+      throw new Error(`Prototype state value ${prop} is not a number`);
+    }
+    if (typeof easing[prop] !== 'function') {
+      if (!easing[prop]) {
+        throw new Error(`Easing function for ${prop} is missing`);
+      } else {
+        throw new Error(`Easing function for ${prop} is not a function`);
+      }
+    }
+  }
+
   return (fromState, toState, position, outputState = { }) => {
     for (let prop in state) {
-      outputState[prop] = interpolate(fromState[prop], toState[prop], position);
+      outputState[prop] =
+        interpolate(fromState[prop], toState[prop], easing[prop](position));
     }
     return outputState;
   };
--- test/createInterpolator.spec.js
+++ test/createInterpolator.spec.js
@@ -22,4 +22,27 @@ describe('createInterpolator', () => {
       .toEqual({ x: 0.5 })
       .toBe(outputState);
   });
+
+  it('calls the easing function', () => {
+    const easing = pos => Math.pow(pos, 2);
+    const interpolator = createInterpolator({ x: 0 }, { x: easing });
+
+    expect(interpolator({ x: 0 }, { x: 1 }, 0.5))
+      .toEqual({ x: 0.25 });
+  });
+
+  it('fails with a missing easing function', () => {
+    expect(() => createInterpolator({ x: 0 }, { y: identity }))
+      .toThrow(/missing/);
+  });
+
+  it('fails with a non-function easing function', () => {
+    expect(() => createInterpolator({ x: 0 }, { x: 'easing' }))
+      .toThrow(/not a function/);
+  });
+
+  it('fails if the shape prototype has bad values', () => {
+    expect(() => createInterpolator({ x: 'what' }, { x: identity }))
+      .toThrow(/not a number/);
+  });
 });
