1d4ba2e72d369988038dcca1dd6195d6e9dc5ee0
Brian Downing <bdowning@lavos.net>
2016-04-19 18:01:40 -0500

    Basic, basic functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ export function identity(x) {
   return x;
 }
 
+function interpolate(from, to, position) {
+  return from + (to - from) * position;
+}
+
 export function createInterpolator(state, easing) {
-  return identity;
+  return (fromState, toState, position, outputState = { }) => {
+    for (let prop in state) {
+      outputState[prop] = interpolate(fromState[prop], toState[prop], position);
+    }
+    return outputState;
+  };
 }
--- test/createInterpolator.spec.js
+++ test/createInterpolator.spec.js
@@ -7,4 +7,19 @@ describe('createInterpolator', () => {
 
     expect(interpolator).toBeA('function');
   });
+
+  it('interpolates a single parameter', () => {
+    const interpolator = createInterpolator({ x: 0 }, { x: identity });
+
+    expect(interpolator({ x: 0 }, { x: 1 }, 0.5)).toEqual({ x: 0.5 });
+  });
+
+  it('destructively mutates outputState', () => {
+    const interpolator = createInterpolator({ x: 0 }, { x: identity });
+    const outputState = { };
+
+    expect(interpolator({ x: 0 }, { x: 1 }, 0.5, outputState))
+      .toEqual({ x: 0.5 })
+      .toBe(outputState);
+  });
 });
