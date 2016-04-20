fe88b535401a2e356b4e83abb4c31aaffb252d17
Brian Downing <bdowning@lavos.net>
2016-04-20 12:59:24 -0500

    Add polyfill

--- src/core.js
+++ src/core.js
@@ -1,3 +1,5 @@
+import './polyfill';
+
 export { identity } from './identity';
 export { createInterpolator } from './createInterpolator';
 export { identityPreprocessor, composePreprocessors } from './composePreprocessors';
