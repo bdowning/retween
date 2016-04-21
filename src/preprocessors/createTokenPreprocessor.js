dc83dba73a8e665a561da716f49b98b8ad1b0eba
Brian Downing <bdowning@lavos.net>
2016-04-20 21:35:23 -0500

    Compiled decoder functions

--- src/preprocessors/createTokenPreprocessor.js
+++ src/preprocessors/createTokenPreprocessor.js
@@ -1,5 +1,35 @@
 import identity from '../identity';
 
+const decodeFunctionCache = { };
+
+function getDecodeFunction(propsInfo, cacheKey) {
+  if (!decodeFunctionCache[cacheKey]) {
+    const code = [ 'return function decode (state) { return {' ];
+
+    for (let propInfo of propsInfo) {
+      const { prop, parts, values } = propInfo;
+      code.push(JSON.stringify(prop));
+      code.push(':');
+      for (let i = 0; i < parts.length; ++i) {
+        if (i > 0) {
+          code.push('+');
+        }
+        if (i % 2 === 1) {
+          const value = values[(i - 1) / 2];
+          code.push(`parseFloat(state[${JSON.stringify(value.name)}]).toFixed(${value.precision})`);
+        } else {
+          code.push(JSON.stringify(parts[i]));
+        }
+      }
+      code.push(',');
+    }
+    code.push('}; };');
+
+    decodeFunctionCache[cacheKey] = new Function(code.join(''))();
+  }
+  return decodeFunctionCache[cacheKey];
+}
+
 export default function createTokenPreprocessor(options = { }) {
   options = Object.assign({
     defaultPrecision: 2
@@ -29,6 +59,7 @@ export default function createTokenPreprocessor(options = { }) {
           value: parts[i],
           precision: options.defaultPrecision,
         });
+        parts[i] = '!!!VAL!!!';
       }
 
       for (let i = 0; i < values.length; ++i) {
@@ -45,29 +76,17 @@ export default function createTokenPreprocessor(options = { }) {
         }
       }
 
+      const cacheKey = parts.join('');
+
       delete newState[prop];
       delete newEasing[prop];
 
-      propsInfo.push({ prop, parts, values });
+      propsInfo.push({ prop, parts, values, cacheKey });
     }
 
-    const decoder = (outputState) => {
-      const inputState = Object.assign({ }, outputState);
+    const cacheKey = propsInfo.map(x => x.cacheKey).join('!!!PROP!!!');
 
-      for (let propInfo of propsInfo) {
-        const { prop, values } = propInfo;
-        const parts = propInfo.parts.slice();
-
-        for (let i = 0; i < values.length; ++i) {
-          parts[i * 2 + 1] = outputState[values[i].name].toFixed(values[i].precision);
-          delete inputState[values[i].name];
-        }
-
-        inputState[prop] = parts.join('');
-      }
-
-      return inputState;
-    };
+    const decoder = getDecodeFunction(propsInfo, cacheKey);
 
     return [ newState, newEasing, decoder ];
   };
