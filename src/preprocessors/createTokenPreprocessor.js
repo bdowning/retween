89ff7fd2b1f040aa5a0569cd664776410146f7ed
Brian Downing <bdowning@lavos.net>
2016-04-21 01:31:36 -0500

    Pass non-modified props through the tokenPreprocessor decoder

--- src/preprocessors/createTokenPreprocessor.js
+++ src/preprocessors/createTokenPreprocessor.js
@@ -7,18 +7,22 @@ function getDecodeFunction(propsInfo, cacheKey) {
     const code = [ 'return function decode (state) { return {' ];
 
     for (let propInfo of propsInfo) {
-      const { prop, parts, values } = propInfo;
+      const { prop, pass, parts, values } = propInfo;
       code.push(JSON.stringify(prop));
       code.push(':');
-      for (let i = 0; i < parts.length; ++i) {
-        if (i > 0) {
-          code.push('+');
-        }
-        if (i % 2 === 1) {
-          const value = values[(i - 1) / 2];
-          code.push(`parseFloat(state[${JSON.stringify(value.name)}]).toFixed(${value.precision})`);
-        } else {
-          code.push(JSON.stringify(parts[i]));
+      if (propInfo.pass) {
+        code.push(`state[${JSON.stringify(prop)}]`);
+      } else {
+        for (let i = 0; i < parts.length; ++i) {
+          if (i > 0) {
+            code.push('+');
+          }
+          if (i % 2 === 1) {
+            const value = values[(i - 1) / 2];
+            code.push(`parseFloat(state[${JSON.stringify(value.name)}]).toFixed(${value.precision})`);
+          } else {
+            code.push(JSON.stringify(parts[i]));
+          }
         }
       }
       code.push(',');
@@ -36,9 +40,9 @@ export default function createTokenPreprocessor(options = { }) {
   }, options);
 
   return (state, easing) => {
-    const props = Object.keys(state).filter(k => typeof state[k] === 'string').sort();
+    const props = Object.keys(state).sort();
 
-    if (props.length === 0) {
+    if (props.filter(k => typeof state[k] === 'string').length === 0) {
       return [ state, easing, identity ];
     }
 
@@ -47,41 +51,45 @@ export default function createTokenPreprocessor(options = { }) {
     const propsInfo = [ ];
 
     for (let prop of props) {
-      const parts = state[prop].split(/(-?\b(?:\d+(?:\.\d+|\.)?|\.\d+))/);
-      const values = [ ];
+      if (typeof state[prop] === 'string') {
+        const parts = state[prop].split(/(-?\b(?:\d+(?:\.\d+|\.)?|\.\d+))/);
+        const values = [ ];
 
-      if (typeof easing[prop] === 'string' && easing[prop].match(/\s/)) {
-        easing[prop] = easing[prop].split(/\s+/);
-      }
+        if (typeof easing[prop] === 'string' && easing[prop].match(/\s/)) {
+          easing[prop] = easing[prop].split(/\s+/);
+        }
 
-      for (let i = 1; i < parts.length; i += 2) {
-        values.push({
-          value: parts[i],
-          precision: options.defaultPrecision,
-        });
-        parts[i] = '!!!VAL!!!';
-      }
+        for (let i = 1; i < parts.length; i += 2) {
+          values.push({
+            value: parts[i],
+            precision: options.defaultPrecision,
+          });
+          parts[i] = prop;
+        }
 
-      for (let i = 0; i < values.length; ++i) {
-        values[i].num = i;
-        values[i].name = prop + '__' + i;
-        newState[values[i].name] = parseFloat(values[i].value);
+        for (let i = 0; i < values.length; ++i) {
+          values[i].num = i;
+          values[i].name = prop + '__' + i;
+          newState[values[i].name] = parseFloat(values[i].value);
 
-        if (easing[prop]) {
-          if (Array.isArray(easing[prop])) {
-            newEasing[values[i].name] = easing[prop][i];
-          } else {
-            newEasing[values[i].name] = easing[prop];
+          if (easing[prop]) {
+            if (Array.isArray(easing[prop])) {
+              newEasing[values[i].name] = easing[prop][i];
+            } else {
+              newEasing[values[i].name] = easing[prop];
+            }
           }
         }
+
+        const cacheKey = parts.join('!!!');
+
+        delete newState[prop];
+        delete newEasing[prop];
+
+        propsInfo.push({ prop, parts, values, cacheKey });
+      } else {
+        propsInfo.push({ prop, pass: true, cacheKey: prop });
       }
-
-      const cacheKey = parts.join('');
-
-      delete newState[prop];
-      delete newEasing[prop];
-
-      propsInfo.push({ prop, parts, values, cacheKey });
     }
 
     const cacheKey = propsInfo.map(x => x.cacheKey).join('!!!PROP!!!');
